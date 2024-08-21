e857d9825ab5230238431ef590a1e76ea193049d
Saul Rodriguez <saulrodriguez221299@gmail.com>
2024-08-21 11:27:10 -0600

    feat: first commit
    Implementation of CRUD with nestjs

---
+++ src/tasks/dto/task.dto.ts
@@ -0,0 +1,26 @@
+import { TaskStatus } from "../task.entity";
+import { IsIn, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+export class CreateTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    title: string;
+    
+    @IsString()
+    description: string;
+}
+
+export class UpdateTaskDto{
+    @IsString()
+    @IsOptional()
+    title?: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.PENDING])
+    status?: TaskStatus;
+}
